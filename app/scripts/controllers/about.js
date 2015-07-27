5760e16d22c8524a2222ec89f43e05fcf2650a81
ignacio.suay <ignacio.suay@gmail.com>
2015-07-27 18:27:19 +0100

    Create cv project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvsuaymasApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cvsuaymasApp
+ */
+angular.module('cvsuaymasApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cvsuaymasApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cvsuaymasApp
+ */
+angular.module('cvsuaymasApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
